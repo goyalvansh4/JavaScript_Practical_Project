0ef0ec797defce5cb6799b7d063090cae2c0a13c
Vansh Gupta <vanshgupta7668@gmail.com>
2023-08-30 11:10:54 +0530

    update another section

--- Project_9/main.js
+++ Project_9/main.js
@@ -41,3 +41,22 @@ storyian.addEventListener("click",function(dets){
         fullScreen.style.display="none";
     },3000)
 })
+
+
+var contain = document.querySelector("#container");
+
+var icon = document.querySelector("i");
+
+contain.addEventListener("dblclick",function(){
+    icon.style.transform = "translate(-50%,-50%) scale(1)"
+    icon.style.opacity=0.8;
+    // icon.style.color="red";
+
+    setTimeout(function(){
+        icon.style.opacity=0;
+    },1000)
+
+    setTimeout(function(){
+        icon.style.transform = "translate(-50%,-50%) scale(0)"
+    },1500)
+});
