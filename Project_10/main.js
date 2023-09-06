de6bf98b0f9b170dddf04ba8804db488f581681d
Vansh Gupta <vanshgupta7668@gmail.com>
2023-09-06 09:34:37 +0530

    Update the openWeather Api and fetch value

---
+++ Project_10/main.js
@@ -0,0 +1,40 @@
+const apiKey ="853ddf22aba8823cc10fa2638a361172";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function chechWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data =  await response.json();
+     console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/hr";
+   
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "./images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "./images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "./images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "./images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "./images/mist.png";
+    }
+
+}   
+
+searchBtn.addEventListener("click",()=>{
+    chechWeather(searchBox.value);
+})
+
+
